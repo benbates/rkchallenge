dbed95373c93d167dcc6c1ea321665cdfb3f4af9
Ben Bates <ben.bates@runkeeper.com>
2014-04-29 22:41:24 -0400

    Show completed goals on a users page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,5 +15,9 @@ module UsersHelper
   def current_goal(user, exercise_type)
     user.goals.where( exercise_type_id: exercise_type, completed_date: nil).first
   end
+
+  def completed_goals(user)
+    user.goals.where( "completed_date IS NOT NULL")
+  end
   
 end
