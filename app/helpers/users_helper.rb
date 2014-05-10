af85129058f96e401e1a56de8dee4b3558aa2b48
Ben Bates <ben.bates@runkeeper.com>
2014-05-10 11:06:13 -0400

    Show completed goals

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,11 +13,18 @@ module UsersHelper
   end
 
   def current_goal(user, exercise_type)
-    user.goals.where( exercise_type_id: exercise_type, completed_date: nil).first
+    user.goals.where(exercise_type_id: exercise_type, completed_date: nil).first
   end
 
   def completed_goals(user)
-    user.goals.where( "completed_date IS NOT NULL")
+    user.goals.where("completed_date IS NOT NULL")
+  end
+
+  def all_completed_goals
+    Goal.where("completed_date IS NOT NULL").order("completed_date DESC")
   end
   
+  def et_completed_goals(exercise_type)
+    Goal.where( "completed_date IS NOT NULL" ).where(exercise_type_id: exercise_type).order("completed_date DESC")
+  end
 end
